17033a034eb73fd406fc1b4d9a5c1160bfba4c32
Xeno Kovah <xkovah@mitre.org>
2007-09-21 16:49:47 +0000

    integrity check piece seems to be parsing and working well enough, and some more stuff filled in in the capture code...now I'm going to see if I can fix the way the exclusion list regex works because of how I broke it due to the splitting the registry path
    git-svn-id: svn+ssh://svn.honeyclient.org/svn/honeyclient/branches/exp/xeno-realtime_integrity/Capture2/capture-client-xeno-mod@834 143ac459-0e48-db11-92d1-000d614347cd

--- RegistryMonitor.cpp
+++ RegistryMonitor.cpp
@@ -263,33 +263,28 @@ RegistryMonitor::run()
 	
 					//Handle all the post-processing to format the data
 					wchar_t szTemp[256];
-					//wstring extraData;
 					wstring other;
+					size_t tmp_len;
 					vector<wstring> extraData;
-					///CopyMemory(szTemp,e->valueName, e->valueNameLength);
-					///szTemp[e->valueNameLength] = '\0';
-					///extraData.push_back(szTemp);
 					extraData.push_back(e->valueName);
+
+
+					//MS description of data types:
+					//http://support.microsoft.com/kb/256986
 					switch(e->dataType){
 						case REG_NONE:
-							//extraData.append(L"REG_NONE - ");
 							extraData.push_back(L"REG_NONE");
-							extraData.push_back(L"");
+							extraData.push_back(L""); //This is so that the logger adds an extra blank value
 							break;
 						case REG_SZ:
-							//extraData.append(L"REG_SZ - ");
 							extraData.push_back(L"REG_SZ");
-							//extraData.append((wchar_t *)registryData);
 							extraData.push_back((wchar_t *)registryData);
 							break;
 						case REG_EXPAND_SZ:
-							//extraData.append(L"REG_EXPAND_SZ - ");
 							extraData.push_back(L"REG_EXPAND_SZ");
-							//extraData.append((wchar_t *)registryData);
 							extraData.push_back((wchar_t *)registryData);
 							break;
 						case REG_BINARY:
-							//extraData.append(L"REG_BINARY - ");
 							extraData.push_back(L"REG_BINARY");
 							//FIXME: Better way?
 							if(e->dataLengthB < 256){
@@ -308,62 +303,61 @@ RegistryMonitor::run()
 							}
 							break;
 						case REG_DWORD:
-							//extraData.append(L"REG_DWORD - ");
 							extraData.push_back(L"REG_DWORD");
 							swprintf_s(szTemp, 256, L"%lx", ((DWORD *)registryData)[0]);
-							//extraData.append(szTemp);
 							extraData.push_back(szTemp);
 							break;
+						//TODO: Untested
 						case REG_DWORD_BIG_ENDIAN:
-							//extraData.append(L"REG_DWORD_BIG_ENDIAN");
 							extraData.push_back(L"REG_DWORD_BIG_ENDIAN");
-							//FIXME: Fill in
-							extraData.push_back(L"FILL IN");
+							swprintf_s(szTemp, 256, L"%x%x%x%x", registryData[0],registryData[1],registryData[2],registryData[3]);
+							extraData.push_back(szTemp);
 							break;
+						//From MS: "A Unicode string naming a symbolic link."
+						//TODO: Untested
 						case REG_LINK:
-							//extraData.append(L"REG_LINK");
 							extraData.push_back(L"REG_LINK");
-							//FIXME: Fill in
-							extraData.push_back(L"FILL IN");
+							extraData.push_back((wchar_t *)registryData);
 							break;
+						//TODO: regedit won't let me make a string,empty string, string, but that 
+						// doesn't mean something else might not be able to do it. Look into it as it would
+						// break the while condition into ending early.
 						case REG_MULTI_SZ:
-							//extraData.append(L"REG_MULTI_SZ");
 							extraData.push_back(L"REG_MULTI_SZ");
-							//FIXME: Fill in
-							extraData.push_back(L"FILL IN");
+							while(((wchar_t *)registryData)[0] != '\0' && ((wchar_t *)registryData)[1] != '\0'){
+								other.append((wchar_t *)registryData);
+								other.append(L"-|-");
+								tmp_len = wcsnlen((wchar_t *)registryData, 512);
+								registryData = (BYTE *)((wchar_t *)registryData + (tmp_len + 1));
+							}
+							extraData.push_back(other);
 							break;
+						//TODO: Untested, "A series of nested arrays..."
 						case REG_RESOURCE_LIST:
-							//extraData.append(L"REG_RESOURCE_LIST");
 							extraData.push_back(L"REG_RESOURCE_LIST");
-							//FIXME: Fill in
 							extraData.push_back(L"FILL IN");
 							break;
+						//TODO: Untested, "A series of nested arrays..."
 						case REG_FULL_RESOURCE_DESCRIPTOR:
-							//extraData.append(L"REG_FULL_RESOURCE_DESCRIPTOR");
 							extraData.push_back(L"REG_FULL_RESOURCE_DESCRIPTOR");
-							//FIXME: Fill in
 							extraData.push_back(L"FILL IN");
 							break;
+						//TODO: Untested, "A series of nested arrays..."
 						case REG_RESOURCE_REQUIREMENTS_LIST:
-							//extraData.append(L"REG_RESOURCE_REQUIREMENTS_LIST");
 							extraData.push_back(L"REG_RESOURCE_REQUIREMENTS_LIST");
-							//FIXME: Fill in
+							extraData.push_back(L"FILL IN");
 							break;
 						case REG_QWORD_LITTLE_ENDIAN:
-							//extraData.append(L"REG_QWORD");
 							extraData.push_back(L"REG_QWORD");
-							//FIXME: Fill in (higher priority, have seen this from VS)
-							extraData.push_back(L"FILL IN");
+							swprintf_s(szTemp, 256, L"%lx%lx", ((DWORD *)registryData)[0],((DWORD *)registryData)[1]);
+							extraData.push_back(szTemp);
 							break;
 						default:
-							//extraData.append(L"UNKNOWN TYPE!");
 							extraData.push_back(L"UNKNOWN TYPE!");
 							swprintf_s(szTemp, 256, L"%ld", e->dataType);
-							//extraData.append(szTemp);
 							extraData.push_back(szTemp);
 							break;
 					}
-
 					signal_onRegistryEvent(registryEventName, time, processPath, registryPath, extraData);
 				}
 				if(registryData != NULL)
